373da832a3e3372f5ca39f40efbd826e16257367
NabilKannane <nabilkannane1@gmail.com>
2024-06-07 01:53:06 +0100

    msg error & success upload

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -144,10 +144,10 @@ const Upload = () => {
           </svg>
         </div>
         <p className="mt-4 py-4  text-violet-300 text-center text-sm ">{fileName}</p>
-        {uploadSuccess && (
+        {uploadSuccess ? (
           <p className="text-green-500">File uploaded successfully!</p>
-        )}
-        {errorUpload && <p className="text-red-500">{errorUpload}</p>}
+        ) : ""}
+        {errorUpload ? <p className="text-red-500">{errorUpload}</p> : ""}
         <div className="flex justify-between mt-4 mb-8 gap-4">
           <button
             className=" shadow-slate-800 shadow-md transition-background inline-flex h-12 items-center justify-center rounded-md  border border-gray-800 bg-gradient-to-r from-[#111111] via-neutral-900 to-[#8c2fff] bg-[length:200%_200%] bg-[0%_0%] px-6 font-medium hover:text-white duration-500 hover:bg-[100%_200%] focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-gray-50"
